550386086b97d901f1a8c2a009e1c8b46ab9d96d
rydfdli <rydfadli@gmail.com>
2025-12-02 19:35:54 +0700

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,12 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css',
-                'resources/css/home.css', 
+                'resources/css/detail.css',
+                'resources/css/home.css',
+                'resources/css/layanan.css',
+                'resources/css/news.css',
+                'resources/css/style.css',
+                'resources/css/visi-misi.css', 
                 'resources/js/app.js',
                 
             ],
